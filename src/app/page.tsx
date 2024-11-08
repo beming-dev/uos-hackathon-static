5400577c5c94ecadb5c3152caec37fe21af2982c
beming-dev <mingfordev@gmail.com>
2024-11-08 21:16:45 +0900

    Add map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <div>hello</div>;
-}
+`use client`;
+
+import KakaoMap from "@/components/KakaoMap";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex">
+      <main className="absolute w-screen h-screen left-0 top-0">
+        <KakaoMap />
+      </main>
+    </div>
+  );
+};
+
+export default Home;
